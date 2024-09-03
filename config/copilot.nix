5f7fad880237effbb9bc9eccccb87e53f577b2db
vr1l <51481761+vr1l@users.noreply.github.com>
2024-09-03 09:44:49 +0200

    testing copilot settings

--- config/copilot.nix
+++ config/copilot.nix
@@ -6,6 +6,7 @@ in{
     enable = true;
     filetypes=  {
       "*" = false;
+      "copilot" = true;
     };
     suggestion = {
       enabled = true;
@@ -16,13 +17,7 @@ in{
     (mkKeymap "n" "<leader>ucc" {
       __raw = #lua
       ''
-        function()
-          if vim.g.copilot_status == nil then
-            vim.g.copilot_status = "running"
-          end
-          if vim.g.copilot_status == "running" then
-            vim.g.copilot_status = "stopped"
-            vim.cmd("Copilot disable")
+        function(ot disable")
           else
             vim.g.copilot_status = "running"
             vim.cmd("Copilot enable")
@@ -31,5 +26,17 @@ in{
       '';
      }
      "Toggle Copilot")
+    (mkKeymap "n" "<leader>ucf" {
+      __raw = #lua
+      ''
+        function ()
+          local original_filetype = vim.bo.filetype
+          vim.bo.filetype = "copilot"
+          vim.cmd('Copilot enable')
+          vim.bo.filetype = original_filetype
+          print("Copilot enabled for this buffer.")
+        end
+      '';
+    } "ForceEnableCopilotForBuffer")
   ];
 }
