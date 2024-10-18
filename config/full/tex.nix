c6f09cd5abab9f68c6bc211356a83e608012ee86
Freimut Diener <51481761+frdiener@users.noreply.github.com>
2024-10-18 23:24:57 +0200

    adaptions to update

--- config/full/tex.nix
+++ config/full/tex.nix
@@ -6,8 +6,21 @@
       enable = true;
       texlivePackage = pkgs.texlive.combined.scheme-full;
     };
+    ltex-extra = {
+      enable = true;
+      settings = {
+        load_langs = [
+          "en-US"
+          "de-DE"
+        ];
+      };
+    };
   };
 
+  extraConfigLua = # lua
+    ''
+      vim.cmd([[let maplocalleader = " t"]])
+    '';
   globals = {
     maplocalleader = " t"; # Set the local leader to "<leader>t"
   };
