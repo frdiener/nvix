a776dfa756e68559aa5434a4cf01be700b8a3c61
Freimut Diener <51481761+frdiener@users.noreply.github.com>
2024-10-18 23:39:35 +0200

    restore ltex customization

--- config/full/tex.nix
+++ config/full/tex.nix
@@ -6,9 +6,23 @@
       enable = true;
       texlivePackage = pkgs.texlive.combined.scheme-full;
     };
+    lsp.servers.ltex = {
+      enable = true;
+      autostart = true;
+      settings = {
+        enabled = true;
+        language = "de-DE";
+        additionalRules = {
+          motherTongue = "de-DE";
+          enablePickyRules = true;
+          completionEnabled = true;
+        };
+      };
+    };
     ltex-extra = {
       enable = true;
       settings = {
+        initCheck = true;
         load_langs = [
           "en-US"
           "de-DE"
