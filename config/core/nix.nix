1246890cb894a370ac89b9b47dad23681c7e8991
Freimut Diener <51481761+frdiener@users.noreply.github.com>
2024-11-28 22:30:27 +0100

    test another way to transparent window

--- config/core/nix.nix
+++ config/core/nix.nix
@@ -51,9 +51,18 @@ with lib;
             on_exit = function(_, code)
               if code ~= 0 then
                 vim.notify("Command exited with code " .. code, vim.log.levels.ERROR)
+              else
+                if vim.api.nvim_win_is_valid(win) then
+                  vim.api.nvim_win_close(win, false)
+                end
+                if vim.api.nvim_buf_is_valid(buf) then
+                  vim.api.nvim_buf_delete(buf, { force = true })
+                end
               end
             end,
           })
+
+          vim.api.nvim_win_set_cursor(0, { 10, 0 })
 
           -- Set keybinding to close the floating window with 'q'
           vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
@@ -67,7 +76,7 @@ with lib;
         function ()
           -- Open a new floating terminal window
           local buf = vim.api.nvim_create_buf(false, true)
-          vim.api.nvim_open_win(buf, true, {
+          local win = vim.api.nvim_open_win(buf, true, {
             relative = 'editor',
             width = 120,
             height = 10,
@@ -75,7 +84,6 @@ with lib;
             row = vim.o.lines - 10,
             border = 'rounded',
             style = 'minimal',
-            bg = 'none',
           })
 
           -- Run the command in the terminal
@@ -83,9 +91,18 @@ with lib;
             on_exit = function(_, code)
               if code ~= 0 then
                 vim.notify("Command exited with code " .. code, vim.log.levels.ERROR)
+              else
+                if vim.api.nvim_win_is_valid(win) then
+                  vim.api.nvim_win_close(win, false)
+                end
+                if vim.api.nvim_buf_is_valid(buf) then
+                  vim.api.nvim_buf_delete(buf, { force = true })
+                end
               end
             end,
           })
+
+          vim.api.nvim_win_set_cursor(0, { 10, 0 })
 
           -- Set keybinding to close the floating window with 'q'
           vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
