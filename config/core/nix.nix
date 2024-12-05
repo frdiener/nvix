15b4022edcb38bae832ddd66481cee588b237205
Freimut Diener <51481761+frdiener@users.noreply.github.com>
2024-12-05 09:26:11 +0100

    fix nh

--- config/core/nix.nix
+++ config/core/nix.nix
@@ -36,7 +36,7 @@ with lib;
         function ()
           -- Open a new floating terminal window
           local buf = vim.api.nvim_create_buf(false, true)
-          vim.api.nvim_open_win(buf, true, {
+          local win = vim.api.nvim_open_win(buf, true, {
             relative = 'editor',
             width = 120,
             height = 10,
